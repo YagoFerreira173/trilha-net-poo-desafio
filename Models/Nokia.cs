0c9315c540e317e274964f40b0892cacaa880c5f
YagoFerreira173 <ferreiracirinoyago@gmail.com>
2025-07-11 18:38:10 -0300

    Atividade Finalizada

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int menoria) : base(numero, modelo, imei, menoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone: Nokia");
+Smartphone Nokia = new Nokia(numero: "123", modelo: "Nokia 3310", imei: "12345", menoria: 32);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Clash Of Clãs");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone: Iphone");
+Smartphone Iphone = new Iphone(numero: "321", modelo: "Iphone 16", imei: "54321", menoria: 255);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Clash Royale");
